66f0e99d4c0310670aaa8c1f7b13f4a4f2486659
LUCIezq <lucieze02@icloud.com>
2026-06-03 17:55:43 -0300

    creacion de tabla usuario

--- Persistence/Migrations/20260603205012_CreacionDeTablaUsuario.Designer.cs
+++ Persistence/Migrations/20260603205012_CreacionDeTablaUsuario.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PracticaParcial.Persistence;
 
@@ -11,9 +12,11 @@ using PracticaParcial.Persistence;
 namespace Consorcio.Entidades.Migrations
 {
     [DbContext(typeof(UnidadDbContext))]
-    partial class UnidadDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260603205012_CreacionDeTablaUsuario")]
+    partial class CreacionDeTablaUsuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Persistence/UnidadDbContext.cs
+++ Persistence/UnidadDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PracticaParcial.Models.Reserva;
 using PracticaParcial.Models.Unidades;
+using PracticaParcial.Models.Users;
 
 namespace PracticaParcial.Persistence;
 
@@ -8,6 +9,8 @@ public class UnidadDbContext : DbContext
 {
     public DbSet<Unidad> Unidades { get; set; }
     public DbSet<ReservaSUM> ReservasSUM { get; set; }
+
+    public DbSet<User> Users { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ConsorcioUnidadDb;Trusted_Connection=True;TrustServerCertificate=True");
