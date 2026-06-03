9abcd2a1342840c0da9658ea5f40ce96e1c72a16
LUCIezq <lucieze02@icloud.com>
2026-06-03 20:20:38 -0300

    agrego consorcio

--- Persistence/Migrations/20260603232025_AgregoConsorcio.Designer.cs
+++ Persistence/Migrations/20260603232025_AgregoConsorcio.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PracticaParcial.Persistence;
 
@@ -11,9 +12,11 @@ using PracticaParcial.Persistence;
 namespace PracticaParcial.Persistence.Migrations
 {
     [DbContext(typeof(UnidadDbContext))]
-    partial class UnidadDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260603232025_AgregoConsorcio")]
+    partial class AgregoConsorcio
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Persistence/Migrations/20260603232025_AgregoConsorcio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PracticaParcial.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregoConsorcio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Consorcios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Calle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ciudad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Provincia = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CodigoPostal = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DiaVencimientoExpensas = table.Column<int>(type: "int", nullable: false),
+                    Latitud = table.Column<double>(type: "float", nullable: false),
+                    Longitud = table.Column<double>(type: "float", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consorcios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consorcios");
+        }
+    }
+}
